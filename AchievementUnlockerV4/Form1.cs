6fe13117b836465bf6a8a872f734dcc2520ce75a
XeCrippy <mevans407.me@gmail.com>
2025-01-12 11:24:54 -0500

    Added avatar awards and gamer pictures

--- AchievementUnlockerV4/Form1.cs
+++ AchievementUnlockerV4/Form1.cs
@@ -15,6 +15,20 @@ namespace AchievementUnlockerV4
             return index + 1;
         }
 
+        void XUserAwardGamerPicture()
+        {
+            uint address = console.ResolveFunction("xam.xex", 0x2F0);
+            uint dwUserIndex = 0;
+            uint numPictures = 20;
+            uint dwPictureId = 0;
+
+            for (uint i = 0; i < numPictures; i++)
+            {
+                dwPictureId = i;
+                console.CallVoid(address, dwUserIndex, 0, dwPictureId | 0x20000, dwPictureId | 0x10000, 1, 0);
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -29,7 +43,7 @@ namespace AchievementUnlockerV4
                 string remoteName = "HDD:\\Cache\\cheesedick.xex";
                 File.WriteAllBytes(localName, file);
                 console.SendFile(localName, remoteName);
-                console.LoadModule("HDD:\\cheesedick.xex");
+                console.LoadModule("HDD:\\Cache\\cheesedick.xex");
 
                 uint achievementPtr = 0x3A168860;
                 uint pOverlapped = 0x3A168840;
@@ -44,7 +58,7 @@ namespace AchievementUnlockerV4
 
                 console.WriteUInt64(achievementPtr, achievements);
 
-                console.CallVoid(0x91E02258, numAchievements, achievementPtr, pOverlapped, pUserBuffer);
+                console.CallVoid(0x91E02340, numAchievements, achievementPtr, pOverlapped, pUserBuffer);
             }
             catch (Exception ex)
             {
@@ -63,6 +77,55 @@ namespace AchievementUnlockerV4
             {
                 if (!console.Connect(out console))
                     MessageBox.Show(Text = "Failed to connect to console");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] file = Properties.Resources.cheesedick;
+                string localName = Path.Combine(Path.GetTempPath(), "cheesedick.tmp");
+                string remoteName = "HDD:\\Cache\\cheesedick.xex";
+                File.WriteAllBytes(localName, file);
+                console.SendFile(localName, remoteName);
+                console.LoadModule("HDD:\\Cache\\cheesedick.xex");
+
+                uint awardsPtr = 0x3A168860;
+                uint pOverlapped = 0x3A168840;
+                uint pUserBuffer = 0x3A168EA0;
+                uint numAwards = 50;
+                ulong[] awards = new ulong[numAwards];
+
+                for (uint i = 0; i < numAwards; i++)
+                {
+                    awards[i] = GetIdFromIndex(i);
+                }
+
+                console.WriteUInt64(awardsPtr, awards);
+
+                console.CallVoid(0x91E023B8, numAwards, awardsPtr, pOverlapped, pUserBuffer);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                File.Delete(Path.Combine(Path.GetTempPath(), "cheesedick.tmp"));
+                console.DeleteFile("HDD:\\Cache\\cheesedick.xex");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                XUserAwardGamerPicture();
             }
             catch (Exception ex)
             {
