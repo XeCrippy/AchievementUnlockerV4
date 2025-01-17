200158bd00f461231597eeb956d20b3b6863d038
XeCrippy <mevans407.me@gmail.com>
2025-01-17 16:23:52 -0500

    *Fixed profile corruption

--- AchievementUnlockerV4/Form1.Designer.cs
+++ AchievementUnlockerV4/Form1.Designer.cs
@@ -41,7 +41,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Unlock Achievements";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.AchievementsBtn_Click);
             // 
             // button2
             // 
@@ -51,7 +51,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "Unlock Avatar Awards";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.AvatarAwardsBtn_Click);
             // 
             // button3
             // 
@@ -61,7 +61,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "Unlock Gamer Pics";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Click += new System.EventHandler(this.GamerPicsBtn_Click);
             // 
             // Form1
             // 
@@ -74,6 +74,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Achievement Unlocker";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
--- AchievementUnlockerV4/Form1.cs
+++ AchievementUnlockerV4/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using JRPCPlusPlus;
 using System;
 using System.IO;
+using System.Threading;
 using System.Windows.Forms;
 using XDevkit;
 
@@ -10,14 +11,57 @@ namespace AchievementUnlockerV4
     {
         IXboxConsole console;
 
-        uint GetIdFromIndex(uint index)
+        void UnlockAchievements()
         {
-            return index + 1;
+            try
+            {
+                byte[] file = Properties.Resources.cheesedick;
+                string localName = Path.Combine(Path.GetTempPath(), "cheesedick.tmp");
+                string remoteName = "HDD:\\cheesedick.xex";
+                File.WriteAllBytes(localName, file);
+                console.SendFile(localName, remoteName);
+                console.LoadModule("HDD:\\cheesedick.xex");
+
+                console.CallVoid(0x91E03000);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                File.Delete(Path.Combine(Path.GetTempPath(), "cheesedick.tmp"));
+                console.DeleteFile("HDD:\\cheesedick.xex");
+            }
         }
 
+        void UnlockAvatarAwards()
+        {
+            try
+            {
+                byte[] file = Properties.Resources.cheesedick;
+                string localName = Path.Combine(Path.GetTempPath(), "cheesedick.tmp");
+                string remoteName = "HDD:\\cheesedick.xex";
+                File.WriteAllBytes(localName, file);
+                console.SendFile(localName, remoteName);
+                console.LoadModule("HDD:\\cheesedick.xex");
+
+                console.CallVoid(0x91E03058);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                File.Delete(Path.Combine(Path.GetTempPath(), "cheesedick.tmp"));
+                console.DeleteFile("HDD:\\cheesedick.xex");
+            }
+        }
+        
         void XUserAwardGamerPicture()
         {
-            uint address = console.ResolveFunction("xam.xex", 0x2F0);
+            uint address = console.ResolveFunction("xam.xex", 0x2F0); // XamWriteGamerTile
             uint dwUserIndex = 0;
             uint numPictures = 20;
             uint dwPictureId = 0;
@@ -26,6 +70,7 @@ namespace AchievementUnlockerV4
             {
                 dwPictureId = i;
                 console.CallVoid(address, dwUserIndex, 0, dwPictureId | 0x20000, dwPictureId | 0x10000, 1, 0);
+                Thread.Sleep(30);
             }
         }
 
@@ -34,41 +79,9 @@ namespace AchievementUnlockerV4
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void AchievementsBtn_Click(object sender, EventArgs e)
         {
-            try
-            {
-                byte[] file = Properties.Resources.cheesedick;
-                string localName = Path.Combine(Path.GetTempPath(), "cheesedick.tmp");
-                string remoteName = "HDD:\\Cache\\cheesedick.xex";
-                File.WriteAllBytes(localName, file);
-                console.SendFile(localName, remoteName);
-                console.LoadModule("HDD:\\Cache\\cheesedick.xex");
-
-                uint achievementPtr = 0x3A168860;
-                uint pOverlapped = 0x3A168840;
-                uint pUserBuffer = 0x3A168EA0;
-                uint numAchievements = 200;
-                ulong[] achievements = new ulong[numAchievements];
-
-                for (uint i = 0; i < numAchievements; i++)
-                {
-                    achievements[i] = GetIdFromIndex(i);
-                }
-
-                console.WriteUInt64(achievementPtr, achievements);
-
-                console.CallVoid(0x91E02340, numAchievements, achievementPtr, pOverlapped, pUserBuffer);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                File.Delete(Path.Combine(Path.GetTempPath(), "cheesedick.tmp"));
-                console.DeleteFile("HDD:\\Cache\\cheesedick.xex");
-            }
+            UnlockAchievements();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -76,7 +89,7 @@ namespace AchievementUnlockerV4
             try
             {
                 if (!console.Connect(out console))
-                    MessageBox.Show(Text = "Failed to connect to console");
+                    MessageBox.Show(Text = "Failed to connect to console!");
             }
             catch (Exception ex)
             {
@@ -84,44 +97,12 @@ namespace AchievementUnlockerV4
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void AvatarAwardsBtn_Click(object sender, EventArgs e)
         {
-            try
-            {
-                byte[] file = Properties.Resources.cheesedick;
-                string localName = Path.Combine(Path.GetTempPath(), "cheesedick.tmp");
-                string remoteName = "HDD:\\Cache\\cheesedick.xex";
-                File.WriteAllBytes(localName, file);
-                console.SendFile(localName, remoteName);
-                console.LoadModule("HDD:\\Cache\\cheesedick.xex");
-
-                uint awardsPtr = 0x3A168860;
-                uint pOverlapped = 0x3A168840;
-                uint pUserBuffer = 0x3A168EA0;
-                uint numAwards = 50;
-                ulong[] awards = new ulong[numAwards];
-
-                for (uint i = 0; i < numAwards; i++)
-                {
-                    awards[i] = GetIdFromIndex(i);
-                }
-
-                console.WriteUInt64(awardsPtr, awards);
-
-                console.CallVoid(0x91E023B8, numAwards, awardsPtr, pOverlapped, pUserBuffer);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                File.Delete(Path.Combine(Path.GetTempPath(), "cheesedick.tmp"));
-                console.DeleteFile("HDD:\\Cache\\cheesedick.xex");
-            }
+            UnlockAvatarAwards();
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void GamerPicsBtn_Click(object sender, EventArgs e)
         {
             try
             {
